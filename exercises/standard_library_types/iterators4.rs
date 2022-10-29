e1d851273685036dd4f30ed6d1959458af7a6c05
YeanSe <1730214056@qq.com>
2022-10-29 14:51:30 +0800

    ✅ done: 75/84 now

--- exercises/standard_library_types/iterators4.rs
+++ exercises/standard_library_types/iterators4.rs
@@ -1,9 +1,10 @@
 // iterators4.rs
 // Execute `rustlings hint iterators4` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 pub fn factorial(num: u64) -> u64 {
+    if num == 0 {
+        return 1;
+    }
     // Complete this function to return the factorial of num
     // Do not use:
     // - return
@@ -13,6 +14,10 @@ pub fn factorial(num: u64) -> u64 {
     // For an extra challenge, don't use:
     // - recursion
     // Execute `rustlings hint iterators4` for hints.
+    (1..num + 1)
+        .into_iter()
+        .reduce(|prev, curr| prev * curr)
+        .unwrap()
 }
 
 #[cfg(test)]
